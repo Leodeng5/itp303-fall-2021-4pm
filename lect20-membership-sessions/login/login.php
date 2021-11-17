d648fab756e8719ceb42b196a22b0e858c8ae3cc
Nayeon Kim <nykim92@gmail.com>
2021-11-16 18:40:19 -0800

    Add lect 20 files

---
+++ lect20-membership-sessions/login/login.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+require '../config/config.php';
+
+	// If we get $_POST['username], it means user tried to submit the login form (as opposed to user just getting to the form page (GET request))
+	if ( isset($_POST['username']) && isset($_POST['password']) ) {
+		// Checking that both username and password was filled out
+		if ( empty($_POST['username']) || empty($_POST['password']) ) {
+
+			$error = "Please enter username and password.";
+
+		}
+		else {
+			// User filled out both username and password so let's connect to the database and see if the username/password combo is correct
+			$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+			if($mysqli->connect_errno) {
+				echo $mysqli->connect_error;
+				exit();
+			}
+			// Hash the user's input and compare this hashed version to the hashed password in the database
+			$passwordInput = hash("sha256", $_POST["password"]);
+
+			$sql = "SELECT * FROM users
+						WHERE username = '" . $_POST['username'] . "' AND password = '" . $passwordInput . "';";
+
+			echo "<hr>" . $sql . "<hr>";
+			
+			$results = $mysqli->query($sql);
+
+			if(!$results) {
+				echo $mysqli->error;
+				exit();
+			}
+
+			// If we get a result back, that means there was a username/pw combo match! 
+			if($results->num_rows > 0) {
+				// echo "logged in!";
+				// Use session to store some simple information that we want to persist throughout the web application
+				$_SESSION["logged_in"] = true;
+				$_SESSION["username"] = $_POST["username"];
+
+				// Redirect the user to the home page
+				header("Location: ../song-db/index.php");
+				
+			}
+			else {
+				$error = "Invalid username or password.";
+			}
+		} 
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login | Song Database</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+	<?php include '../song-db/nav.php'; ?>
+	<div class="container">
+		<div class="row">
+			<h1 class="col-12 mt-4 mb-4">Login</h1>
+		</div> <!-- .row -->
+	</div> <!-- .container -->
+
+	<div class="container">
+
+		<form action="login.php" method="POST">
+
+			<div class="row mb-3">
+				<div class="font-italic text-danger col-sm-9 ml-sm-auto">
+					<!-- Show errors here. -->
+					<?php
+						if ( isset($error) && !empty($error) ) {
+							echo $error;
+						}
+					?>
+				</div>
+			</div> <!-- .row -->
+			
+
+			<div class="form-group row">
+				<label for="username-id" class="col-sm-3 col-form-label text-sm-right">Username:</label>
+				<div class="col-sm-9">
+					<input type="text" class="form-control" id="username-id" name="username">
+				</div>
+			</div> <!-- .form-group -->
+
+			<div class="form-group row">
+				<label for="password-id" class="col-sm-3 col-form-label text-sm-right">Password:</label>
+				<div class="col-sm-9">
+					<input type="password" class="form-control" id="password-id" name="password">
+				</div>
+			</div> <!-- .form-group -->
+
+			<div class="form-group row">
+				<div class="col-sm-3"></div>
+				<div class="col-sm-9 mt-2">
+					<button type="submit" class="btn btn-primary">Login</button>
+					<a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" role="button" class="btn btn-light">Cancel</a>
+				</div>
+			</div> <!-- .form-group -->
+		</form>
+
+		<div class="row">
+			<div class="col-sm-9 ml-sm-auto">
+				<a href="register_form.php">Create an account</a>
+			</div>
+		</div> <!-- .row -->
+
+	</div> <!-- .container -->
+</body>
+</html>
